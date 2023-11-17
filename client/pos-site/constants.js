4c4a6c49172c65787dfce20ded582490bff0c710
Mohamad Tantyo Wisnuwardhana <tantyowisnu@gmail.com>
2023-11-17 10:25:40 +0700

    deploy client on firebase

--- client/pos-site/constants.js
+++ client/pos-site/constants.js
@@ -1,8 +1,8 @@
 
 // development
-const url = 'http://localhost:3000'
+// const url = 'http://localhost:3000'
 
 // production
-// const url = 'https://groceries-pos-api.tantyowisnu.my.id'
+const url = 'https://groceries-pos-api.tantyowisnu.my.id'
 
 export default url;
